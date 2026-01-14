3a00e167bd7a61e5d124a21ef71acc64f28eb713
matteolpera <matteo.lepera02@gmail.com>
2026-01-14 15:41:34 +0100

    feat: aggiunto il 4

--- query_group.sql
+++ query_group.sql
@@ -5,11 +5,12 @@ GROUP BY `year`
 ORDER BY `year`;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-SELECT (`office_address`), COUNT(*) AS `total_teachers`
+SELECT `office_address`, COUNT(*) AS `total_teachers`
 FROM `teachers`
-GROUP BY `office_address`
+GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT `exam_id`, AVG(`vote`) FROM `exam_student` GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
