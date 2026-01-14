3103ef8c640de15da4035365893888c1e087b4e7
matteolpera <matteo.lepera02@gmail.com>
2026-01-14 15:34:20 +0100

    feat: aggiunto 1-2 group by.

---
+++ query_group.sql
@@ -0,0 +1,15 @@
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT YEAR(`enrolment_date`) as `year`, COUNT(*) as `total_students`
+FROM `students`
+GROUP BY `year`
+ORDER BY `year`;
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT (`office_address`), COUNT(*) AS `total_teachers`
+FROM `teachers`
+GROUP BY `office_address`
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
