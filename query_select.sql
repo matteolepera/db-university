8b224737ab70060c21633be295ac18be56134451
matteolpera <matteo.lepera02@gmail.com>
2026-01-14 15:01:08 +0100

    feat: aggiunte tutte le altre interrogazioni

--- query_select.sql
+++ query_select.sql
@@ -11,18 +11,34 @@ WHERE `cfu` > 10
 ORDER BY `cfu` ASC;
 
 --3. Selezionare tutti gli studenti che hanno più di 30 anni
-
+SELECT *
+FROM `students`
+WHERE TIMESTAMPDIFF(YEAR,`date_of_birth`, CURDATE()) = 30
+ORDER BY `date_of_birth` ASC;
 
 --4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-
+SELECT * 
+FROM `courses`
+WHERE `period` = "I semestre" and `year` = 1
+ORDER BY `name` ASC;
 
 --5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
-
+SELECT * 
+FROM `exams`
+WHERE `date` = "2020-06-20"
+AND TIME(`hour`) > "14";
 
 --6. Selezionare tutti i corsi di laurea magistrale (38)
-
+SELECT * 
+FROM `degrees`
+WHERE `name` LIKE "Corso di Laurea Magistrale%"
+ORDER BY `name` ASC;
 
 --7. Da quanti dipartimenti è composta l'università? (12)
-
+SELECT COUNT(*) AS `total_departments`
+FROM `departments`;
 
 --8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT COUNT(*) AS `teachers_without_phone`
+FROM `teachers`
+WHERE `phone` IS NULL;
