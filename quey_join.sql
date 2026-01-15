847228ca48fe1c3f68df5dad5bd948e512ea9f90
matteolpera <matteo.lepera02@gmail.com>
2026-01-15 15:00:31 +0100

    feat: aggiunti 2-3

--- quey_join.sql
+++ quey_join.sql
@@ -23,13 +23,32 @@ WHERE `degrees`.`name` = "Corso di Laurea in Lettere";
 
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 -- students.* | degrees.name
-
+SELECT `students`.*, `degrees`.`name` AS `degree_name`
+FROM `students`
+INNER JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` = "Corso di Laurea in Economia";
 
 -- 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+-- degrees.name | deparments.name
+SELECT `degrees`.`name`, `departments`.`name`
+FROM `degrees`
+INNER JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `degrees`.level = "magistrale"
+AND `departments`.name = "Dipartimento di Neuroscienze";
 
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
-
+-- courses | teachers
+SELECT `teachers`.name AS `teacher_name`, `teachers`.surname AS `teacher_surname`, `courses`.name AS `course_name`, `courses`.description
+FROM `teachers`
+INNER JOIN `course_teacher`
+ON `course_teacher`.`teacher_id` = `teachers`.id
+INNER JOIN `courses`
+ON `course_teacher`.`course_id` = `courses`.id
+WHERE `teachers`.id = 44
+ORDER BY `course_name` ASC;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
